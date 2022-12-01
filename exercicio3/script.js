9aa703b427f0f93d19353c0a26c87e69534c7448
JuliaGM1993 <juliagm1993@gmail.com>
2022-11-30 23:56:46 -0300

    condicionais I exercícios

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,30 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = [
+    `brasileira`,
+    `argentina`,
+    `uruguaia`,
+    `chilena`,
+    `colombiana`
+]
+
+const suaNacionalidade = prompt("Qual sua nacionalidade?").toLowerCase()
+
+const exercicio3 = suaNacionalidade =>{
+if(suaNacionalidade === nacionalidade[0]){
+        return nacionalidade[0]
+    }else if
+    (suaNacionalidade === nacionalidade[1]){
+        return nacionalidade[1]
+    }else if 
+    (suaNacionalidade === nacionalidade[2]){
+        return nacionalidade[2]
+    }else if
+    (suaNacionalidade === nacionalidade[3]){
+        return nacionalidade[3]
+    }else if
+    (suaNacionalidade === nacionalidade[4]){
+        return nacionalidade[4]
+    }else
+        return `Sua nacionalidade não foi encontrada`
+}
+
+console.log(`Sua nacionalidade é:`,exercicio3(suaNacionalidade))
